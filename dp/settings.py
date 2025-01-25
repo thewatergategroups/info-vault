6d91cdf9dcc74a8cb6ee7410408e6315cc406b6b
Boris Boychev <bobiboychev@gmail.com>
2025-01-25 22:45:57 +0000

    Add full storage settings with minio support

--- dp/settings.py
+++ dp/settings.py
@@ -4,13 +4,26 @@ Application Settings
 
 from functools import lru_cache
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     """Application Settings"""
 
     log_level: str = "INFO"
+    LOG_LEVEL: str = "INFO"
+
+    DATABASE_URL: str = "postgresql://user:pass@localhost/docmanager"
+
+    MINIO_ENDPOINT: str = "localhost:9000"
+    MINIO_ACCESS_KEY: str = "minioadmin"
+    MINIO_SECRET_KEY: str = "minioadmin"
+    MINIO_BUCKET: str = "document-storage"
+    model_config = SettingsConfigDict(env_file=".env")
+
+    # JWT_SECRET_KEY: str
+    # JWT_ALGORITHM: str = "HS256"
+    # ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
 
 @lru_cache
